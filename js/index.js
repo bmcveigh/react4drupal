ad7cb9db8bc84cd8d843d22ec03cc3d0ff76397d
Brian McVeigh <bmcveigh@forumone.com>
2019-01-03 13:43:18 -0500

    Added some boilerplate example React code.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
+class ReactJsLabel extends React.Component {
+
+  state = {
+    isOpen: false
+  };
+
+  handleClick() {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
+  render() {
+    let reactContents = null;
+
+    if (this.state.isOpen) {
+      reactContents = (
+        <div className={"react-contents"}>
+          <h3>I am what you see when React is clicked!</h3>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div onClick={this.handleClick.bind(this)}>
+          <h1>This is ReactJS! Click me!</h1>
+        </div>
+        {reactContents}
+      </div>
+    );
+  }
+
+}
+
 // Create a new component that displays the content of the node.
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is ReactJS!</h1>
+        <ReactJsLabel />
       </div>
     );
   }
