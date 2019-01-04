0bcfc87f8c81a7e891169878a5538be9e4309bd8
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-04 12:46:35 -0500

    Modified gulpfile.

--- modules/react4drupal_example/app/gulpfile.js
+++ modules/react4drupal_example/app/gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  run = require('gulp-run');
+const { parallel } = require('gulp');
+const run = require('gulp-run');
+const watch = require('gulp-watch');
 
-gulp.task('default', function () {
+function js() {
   // Endless stream mode
   return watch('src/**/*.js', { ignoreInitial: false })
     .pipe(run('npm run build'));
-});
+}
+
+exports.js = js;
+exports.default = parallel(js);
